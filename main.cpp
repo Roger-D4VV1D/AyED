e9b9894d5cf4d6a7bbc30cbd34a8bfb4e2d2e7ab
Roger David Dueñez Mejia <rduenezmejia@frba.utn.edu.ar>
2026-05-06 12:53:29 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,15 @@ using namespace std;
 
 int main()
 {
-    std::string nombre;
-    std::cout <<"Ingrese su nombre: ";
+    std::string nombre , apellido;
 
+    std::cout <<"Ingrese su nombre: ";
     std::cin >> nombre;
-    std::cout << "Hola " << nombre << std::endl;
+
+    std::cout <<"Ingrese su apellido: ";
+    std::cin >> apellido;
+
+    std::cout << "Hola " << nombre  << " " << apellido << '\n' << std::endl;
 
     return 0;
 }
